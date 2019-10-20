8881ee68417e6b5fe702453148f948fff269fc3b
Jonathan Liu <jonathancliu168@gmail.com>
2019-10-19 20:08:23 -0500

    Small fixes

--- LHT/main.py
+++ LHT/main.py
@@ -3,7 +3,7 @@ games = open(r"2019 Home Schedule")
 dict_games = dict()
 day = games.readline()
 
-while day != None:
+while day:
     key = day[0:2]+day[3:5]
     dict_games[key] = (day[9:11]+day[12:14])
     day = games.readline()
@@ -11,14 +11,14 @@ while day != None:
 games.close()
 
 # Testing if it's gameday
-gameDay = false
+gameDay = False
 if date == dict_games:
-    gameDay = true
+    gameDay = True
 
 # Acting on whether or not it's gameday
-if gameDay == false:
-    print "No change to Schedule"
+if gameDay == False:
+    print("No change to Schedule")
 else:
-    print "Optimized Schedule From "
+    print("Optimized Schedule From ")
     print (startTime + offset) + " to " + (offset+range)
     display(bus_times)
