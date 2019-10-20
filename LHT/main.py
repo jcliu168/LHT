3c79b1baf7e8a4d376c10f5aba6f27744c64d0e8
Jonathan Liu <jonathancliu168@gmail.com>
2019-10-19 22:33:41 -0500

    Added user input

--- LHT/main.py
+++ LHT/main.py
@@ -1,27 +1,40 @@
+import constants
+import re
+import datetime
+
 # Open file of games and format into dictionary
 games = open(r"2019 Home Schedule")
-dict_games = dict()
 day = games.readline()
+game_sched = dict()
 
 while day:
-    key = day[0:2]+day[3:5]
-    dict_games[key] = (day[9:11]+day[12:14])
+    key = day[0:2] + day[3:5]
+    game_sched[key] = (day[9:11] + day[12:14])
     day = games.readline()
 
 games.close()
 
 # Getting user input on date
+date = None
+while date != "exit":
+    date = input("Date (MM/DD): ")
+    if date.lower() == "exit":
+        print("Bye! :)")
+        break
+    if not re.match(r"^[0-9]{2}[/][0-9]{2}", date):
+        print("Invalid format")
+        continue
+    try:
+        datetime.datetime(2019, int(date[0:2]), int(date[3:5]))
+    except:
+        print("Invalid date")
+        continue
+    date = date[0:2] + date[3:5]
+    game_day = True if date in game_sched else False
+    if not game_day:
+        print("No change to schedule")
+    else:
+        print("Optimized Schedule From ")
+        continue
 
-# Testing if it's gameday
-gameDay = False
-if date == dict_games:
-    gameDay = True
-
-# Acting on whether or not it's gameday
-offset = 200
-if gameDay == False:
-    print("No change to Schedule")
-else:
-    print("Optimized Schedule From ")
-    print (startTime + offset) + " to " + (offset+range)
-    display(bus_times) #needs to be written, function that will do most work
+    #print(startTime + constants.OFFSET) + " to " + (constants.OFFSET + range)
